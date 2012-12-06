f77419cbf6cca469a3f0edb1d7f4379a2d811bb2
Jeff Pollard <jeff.pollard@gmail.com>
2012-12-05 17:49:49 -0800

    0.4.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if 'nosetests' in sys.argv[1:]:
 
 setup(
     name='exam',
-    version='0.4.1',
+    version='0.4.2',
     author='Jeff Pollard',
     author_email='jeff.pollard@gmail.com',
     url='https://github.com/fluxx/exam',
